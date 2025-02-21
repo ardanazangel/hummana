b8ac94f94a7b7aba802a2e1541ae01c43983b23b
aardanazz <83085691+aardanazz@users.noreply.github.com>
2025-02-21 13:44:12 +0100

    cambios en mentorship

--- src/app/mentorship/page.js
+++ src/app/mentorship/page.js
@@ -10,6 +10,8 @@ export default function Page() {
       <BannerProducts
         bannerImage={"/images/mentorship/banner4.jpg"}
         bannerLink={"/hello"}
+        bannerLink2={"/broski"}
+        bannerLinkLabel2={"Come Create"}
         bannerLinkLabel={"Come Create"}
         title={"Mentorship"}
         subtitleLine1={
@@ -39,39 +41,48 @@ export default function Page() {
         subt1={"The past"}
         desc1={
           <>
-            La adversidad es una oportunidad porque nos obliga a ir más
-            profundo, nos empuja a despertar y a hacernos preguntas
-            significativas.
-            <br />
-            <br />A través de HUMMANA mentorship vas a impulsar tu resiliencia y
-            vas a desarrollar tu capacidad de respuesta ante el cambio
+            <p>
+              La adversidad es una oportunidad porque nos obliga a ir más
+              profundo, nos empuja a despertar y a hacernos preguntas
+              significativas.
+            </p>
+            <p>
+              A través de HUMMANA mentorship vas a impulsar tu resiliencia y vas
+              a desarrollar tu capacidad de respuesta ante el cambio.
+            </p>
           </>
         }
         titulo2={"Being in full power"}
         subt2={"The present"}
         desc2={
           <>
-            La mayoría de personas viven entre dos planos imaginarios: el
-            pasado, que no pueden cambiar o el futuro, que no pueden predecir.
-            <br />
-            <br />A través de HUMMANA mentorship agudizarás la escucha y la
-            reflexión sobre tu presente. Además, cultivarás la claridad y el
-            discernimiento necesarios para tu crecimiento y evolución
-            espiritual.
+            <p>
+              La mayoría de personas viven entre dos planos imaginarios: el
+              pasado, que no pueden cambiar o el futuro, que no pueden predecir.
+            </p>
+            <p>
+              A través de HUMMANA mentorship agudizarás la escucha y la
+              reflexión sobre tu presente. Además, cultivarás la claridad y el
+              discernimiento necesarios para tu crecimiento y evolución
+              espiritual.
+            </p>
           </>
         }
         titulo3={"Being rewired"}
         subt3={"The future"}
         desc3={
           <>
-            Para convertir una visión en una realidad es necesario desarrollar
-            una mentalidad específica (growth mindset). Un visionario es aquel
-            capaz de ver más allá del presente, de imaginar un futuro mejor y
-            más brillante. 
-            <br />
-            <br />A través de HUMMANA mentorship aprenderás a implementar un
-            mindset de crecimiento, a superar el burnout y a cultivar un
-            lenguaje alineado con tus metas y con tus objetivos. 
+            <p>
+              Para convertir una visión en una realidad es necesario desarrollar
+              una mentalidad específica (growth mindset). Un visionario es aquel
+              capaz de ver más allá del presente, de imaginar un futuro mejor y
+              más brillante.
+            </p>
+            <p>
+              A través de HUMMANA mentorship aprenderás a implementar un mindset
+              de crecimiento, a superar el burnout y a cultivar un lenguaje
+              alineado con tus metas y con tus objetivos.
+            </p>
           </>
         }
       />
