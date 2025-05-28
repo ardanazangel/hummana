da9407bb0dc25f04c9645011b6e4edf0fc7f0fdf
ardanazangel <angelardanaz2@gmail.com>
2025-05-28 12:07:27 +0200

    cambio en el zindex del video

--- src/app/components/HeroSumba/index.js
+++ src/app/components/HeroSumba/index.js
@@ -97,8 +97,8 @@ export default function HeroSumba() {
         <div id="hero-retreats-content">
           <Image
             src={"/assets/hero-creative.webp"}
-            width={600}
-            height={300}
+            width={1920}
+            height={1080}
             alt="Imagen de fondo retreats"
             ref={imagen}
           />
